705ce7cb1f1d9dd2a8b3d47fc617fb20197f4d63
ushaniar <aushani@umassd.edu>
2014-11-24 20:45:13 -0500

    modified:   bin/Batter.class 	modified:   src/Team.java 	bin/Game.class 	bin/Pitcher.class 	bin/Player.class 	bin/Sim.class 	bin/Team.class

--- src/Team.java
+++ src/Team.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * This class represents a baseball team.
  * 
@@ -6,5 +9,10 @@
  */
 
 public class Team {
-
+	
+	List<Player> Roster = new LinkedList<Player>();
+	
+	String team_Name;
+	
+	
 }
